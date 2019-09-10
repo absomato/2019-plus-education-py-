7ecfc2610ace3ddcb6043675bbad1efcb6f98f2c
absomato <absomato@naver.com>
2019-09-11 01:42:41 +0900

    init

---
+++ 0831/문자열.py
@@ -0,0 +1,11 @@
+print("{:s}".format('i\'m enjoying learning Pyton.'))
+
+print("{0:s}".format("This is a long string. Without the backstash\
+it would run off of the page on the right in the text editor and be very\
+difficult in read and edit.By using the backslash you can split the long\
+string into smaller strings on seperate lines so that the whole string is easy\
+to view in the editor."))
+
+print("{0:s}".format("'Uou can yse triple single quotes for multi-line comment string.'"))
+
+print("{:s}".format("""Uou can also use triple double quotes for multi-line comment string."""))
