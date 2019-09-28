91427f0e8d9d679a571a1c28486029d894a430e7
absomato <44826520+absomato@users.noreply.github.com>
2019-09-28 16:36:57 +0900

    0928 수업

---
+++ 0928/Test01.py
@@ -0,0 +1,21 @@
+list1 = [1,2,3]
+list2 = [4,5,6]
+list3 = [7,8,9]
+list4 = list1+list2+list3
+
+
+for v in range(len(list1)):
+    print('{}'.format(list1[v]))
+
+print ()
+for v in range(len(list2)):
+    print (list2[v])
+
+print ()
+for v in range(len(list3)):
+    print (list3[v])
+
+print ()
+
+for v in range(len(list4)):
+    print (list4[v])
